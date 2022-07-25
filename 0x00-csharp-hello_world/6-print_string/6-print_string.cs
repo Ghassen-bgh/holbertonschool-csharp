d6732e253245116cd963a35bbe3133f2f5db0372
Ghassen-bgh <3591@holbertonschool.com>
2022-07-25 22:08:56 +0200

    Print string

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string a = new StringBuilder(str.Length * 3).Insert(0, str, 3).ToString();
+        string b = str.Substring(0, 9);
+		Console.WriteLine("{0}\n{1}", a, b);        
+    }
+}
