70e22efeeb77d7b2eb610b4528ee9a070fce5415
Ghassen-bgh <3591@holbertonschool.com>
2022-07-26 04:52:05 +0200

    last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            if (number < 0)
+            {
+                number = -number;
+            }
+            int lastDigit = number % 10;
+            Console.Write(lastDigit);
+            return lastDigit;
+        }
+    }
