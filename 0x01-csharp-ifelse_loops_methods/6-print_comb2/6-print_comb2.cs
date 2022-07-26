5028f9a880e7b773962cbf7a58971566f2edab2a
Ghassen-bgh <3591@holbertonschool.com>
2022-07-26 04:14:45 +0200

    comb2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            for (a = 0; a < 9; a++)
+            {
+                for (b = 0; b <= 9; b++)
+                {
+                    if (a < b)
+                    {
+                        if (a < 8 || b < 9)
+                        {
+                            Console.Write($"{a}{b}, ");
+                        }
+                        else
+                        {
+                            Console.Write($"{a}{b}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
