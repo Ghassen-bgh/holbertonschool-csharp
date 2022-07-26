973d913a69bf2f5a65d8008c9e8eff9e1a0d717d
Ghassen-bgh <3591@holbertonschool.com>
2022-07-26 03:49:13 +0200

    comb print

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (number = 0; number <= 99; number++)
+            {
+                if (number == 99)
+                {
+                    Console.Write("{0}", number.ToString("d2"));
+                }
+                else
+                {
+                Console.Write("{0}, ", number.ToString("d2"));
+                }
+            }
+        }
+    }
+}
