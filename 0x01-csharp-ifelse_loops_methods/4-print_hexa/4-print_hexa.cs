c79dff5e1404db555b5abfe6ff9ab43649695fd6
Ghassen-bgh <3591@holbertonschool.com>
2022-07-26 03:39:47 +0200

    hexa

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (number = 0; number <= 98; number++)
+            {
+                Console.WriteLine("{0} = 0x{1}", number, number.ToString("x"));
+            }
+        }
+    }
+}
