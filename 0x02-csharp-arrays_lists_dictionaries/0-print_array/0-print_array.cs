ac0ae9d6a2aaabc6e04466f5f14e85c2db504256
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 14:37:37 +0200

    print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            int[] newArray = new int[size];
+            if (size > 0)
+            {
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    newArray[i] = i;
+                    if (i < size -1)
+                    {
+                        Console.Write(newArray[i] + " ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(newArray[i]);
+                    }
+                }
+            }
+            else if( size == 0)
+            {
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            return newArray;
+        }
+    }
