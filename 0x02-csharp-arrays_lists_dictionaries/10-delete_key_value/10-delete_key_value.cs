f28a4cba84c81b263569742499fa0a42ed8866f5
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 17:30:57 +0200

    delete key value

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+        {
+            Dictionary<string, string> newDict = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string> kvp in myDict)
+            {
+                if (kvp.Key != key)
+                {
+                    newDict.Add(kvp.Key, kvp.Value);
+                }
+            }
+            return newDict;
+        }
+    }
