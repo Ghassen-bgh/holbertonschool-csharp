c3d362f835dd547570a767867cc62e9dc8471bbe
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 17:52:50 +0200

    best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            string best = "";
+            int bestScore = 0;
+            foreach (KeyValuePair<string, int> entry in myList)
+            {
+                if (entry.Value > bestScore)
+                {
+                    best = entry.Key;
+                    bestScore = entry.Value;
+                }
+            }
+            return best;    
+        }
+    }
+
