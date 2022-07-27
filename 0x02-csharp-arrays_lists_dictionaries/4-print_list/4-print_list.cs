cb2ef6a20cdb0d96ce1f8664b55f3332bd712947
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 16:31:59 +0200

    print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            List<int> newList = new List<int>();
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            if (size > 0)
+            {
+                int i = 0;
+                for(; i < size - 1; i++)
+                {
+                    newList.Add(i);
+                    System.Console.Write("{0} ",i);
+                }
+
+                newList.Add(i);
+                System.Console.WriteLine("{0}",i);
+            }
+            else{
+                Console.WriteLine();
+            }
+            return  newList;
+        }
+    }
