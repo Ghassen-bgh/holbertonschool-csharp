08824eb04746a7f8c4d6e1e8aa0824038d332b5e
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 14:47:24 +0200

    fix print

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,32 +4,29 @@
     {
         public static int[] CreatePrint(int size)
         {
-            int[] newArray = new int[size];
-            if (size > 0)
-            {
-                for (int i = 0; i < newArray.Length; i++)
-                {
-                    newArray[i] = i;
-                    if (i < size -1)
-                    {
-                        Console.Write(newArray[i] + " ");
-                    }
-                    else
-                    {
-                        Console.WriteLine(newArray[i]);
-                    }
-                }
-            }
-            else if( size == 0)
+            if( size == 0)
             {
                 Console.WriteLine();
-
+                return new int[size];
             }
-            else
+            else if (size < 0)
             {
                 Console.WriteLine("Size cannot be negative");
                 return null;
             }
-            return newArray;
+            else
+            {
+                int[] newArray = new int[size];
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    newArray[i] = i;
+                    if (i < size - 1)
+                        Console.Write(newArray[i] + " ");
+                    else
+                        Console.WriteLine(newArray[i]);
+                }
+                Console.WriteLine();
+                return newArray;
+            }
         }
     }
