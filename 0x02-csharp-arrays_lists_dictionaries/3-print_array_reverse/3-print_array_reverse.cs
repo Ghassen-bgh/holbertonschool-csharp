37a6a1fbb460dc6fd0840ab14239465f7472723b
Ghassen-bgh <3591@holbertonschool.com>
2022-07-27 16:11:38 +0200

    reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array == null)
+            {
+                Console.WriteLine();
+                return;
+            }
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = temp;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < array.Length - 1)
+                    Console.Write(array[i] + " ");
+                else
+                    Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
