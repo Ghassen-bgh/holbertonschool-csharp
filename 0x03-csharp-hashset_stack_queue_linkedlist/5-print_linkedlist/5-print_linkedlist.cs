f0d0910cca3f5a89db24f7901e57d3073fc9045b
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 15:35:35 +0200

    linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class LList
+    {
+        public static LinkedList<int> CreatePrint(int size)
+        {
+            LinkedList<int> myList = new LinkedList<int>();
+            for (int i = 0; i < size; i++)
+            {
+                myList.AddLast(i);
+            }
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            return myList;
+        }
+    }
