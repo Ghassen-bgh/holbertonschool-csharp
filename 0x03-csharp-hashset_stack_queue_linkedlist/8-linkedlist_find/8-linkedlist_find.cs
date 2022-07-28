431a7b95b1dd3ceef69889ac5b35efc546e9be34
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 15:51:08 +0200

    find

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class LList
+    {
+        public static int FindNode(LinkedList<int> myLList, int value)
+        {
+            LinkedListNode<int> current = myLList.First;
+            int pos = 0;
+            while (current != null)
+            {
+                if (current.Value == value)
+                    return pos;
+                current = current.Next;
+                pos++;
+            }
+            return -1;
+        }
+    }
+
