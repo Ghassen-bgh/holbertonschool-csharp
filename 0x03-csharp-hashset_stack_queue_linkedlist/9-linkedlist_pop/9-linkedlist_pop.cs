61d2282a66d48045da3fb49485dce9bcecd0db58
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 15:56:41 +0200

    pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class LList
+    {
+        public static int Pop(LinkedList<int> myLList)
+        {
+            int popped = myLList.First.Value;
+            if (myLList.Count == 0)
+            {
+                return 0;
+            }
+            myLList.RemoveFirst();
+            return popped;
+        }
+    }
