b3acb1bd5374c3cc040018813aa230b8df637133
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 15:19:54 +0200

    stack push pop fix 4

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,15 +7,12 @@ using System.Collections.Generic;
         public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
         {
             Console.WriteLine($"Number of items: {aStack.Count}");
-            if (aStack.Count == 0)
+            if (aStack.Count != 0)
             {
-                Console.WriteLine("Stack is empty");
-                Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
+                Console.WriteLine($"Top item: {aStack.Peek()}");
             }
             else
-            
-                Console.WriteLine($"Top item: {aStack.Peek()}");
-            
+                Console.WriteLine("Stack is empty");
             Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
 
             while (aStack.Contains(search))
