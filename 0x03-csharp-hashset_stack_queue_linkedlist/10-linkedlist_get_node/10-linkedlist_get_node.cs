a773883f4d5b521c4acf0175bc6b20d425c298b5
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 16:27:27 +0200

    get node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            int i = 0;
+            foreach (var item in myLList)
+            {
+                if (i == n)
+                {
+                    return item;
+                }
+                i++;
+            }
+            return 0;
+        }
+    }
+
