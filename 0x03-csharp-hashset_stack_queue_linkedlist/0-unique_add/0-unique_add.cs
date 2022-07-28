62c44b1d7b1beee61ee756402a5bb0beed5d3561
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 12:29:12 +0200

    unique sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            HashSet<int> mySet = new HashSet<int>();
+            foreach(int item in myList)
+            {
+                mySet.Add(item);
+            }
+            int sum = 0;
+            foreach(int item in mySet)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
