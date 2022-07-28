29d3f37b4b847d2c6334932166bb2e7abaf9a8c4
Ghassen-bgh <3591@holbertonschool.com>
2022-07-28 10:29:59 +0200

    5x5 array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[5, 5];
+            for( int i = 0; i < 5; i++)
+            {
+                for( int j = 0; j < 5; j++)
+                {
+                    if (j == 4)
+                    {
+                        Console.Write("{0}", myArray[i, j]);
+                    }
+                    else if (i == 2 && j == 2)
+                    {
+                        myArray[i, j] = 1;
+                        Console.Write("{0} ", myArray[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", myArray[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
