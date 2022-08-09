e6b514cd05ffb64e0c630ce437b834611b986002
Ghassen-bgh <3591@holbertonschool.com>
2022-08-09 13:22:06 +0200

    zombie name

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Class Zombie.
+    /// </summary>
+     class Zombie
+    {
+        public int health;
+        public string name = "(No name)";
+        public Zombie()
+        {
+            health = 0;
+            
+        }
+        public Zombie(int value)
+        {
+            health = value;
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int GetHealth()
+        {
+            return health;
+        }
+
+    }
+}
