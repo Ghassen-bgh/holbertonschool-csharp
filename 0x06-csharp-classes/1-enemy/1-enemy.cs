b773ec8df54ee5aad1b6d22df9f0884e4d17f214
Ghassen-bgh <3591@holbertonschool.com>
2022-08-09 13:01:56 +0200

    health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Class Zombie.
+    /// </summary>
+     class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
