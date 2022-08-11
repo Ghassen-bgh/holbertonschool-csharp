214963f831e6b729e06781c01b5c1427d2902a3d
Ghassen-bgh <3591@holbertonschool.com>
2022-08-11 15:11:27 +0200

    add int

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(4, 6)]
+        [TestCase(3, 7)]
+        [TestCase(5, 5)]
+        [TestCase(10, 0)]
+        [TestCase(20, -10)]
+
+        public void Add(int a, int b)
+        {
+            int sum = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(a + b, sum);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Mathematical operations.
+    /// </summary>
+    class Operations
+    {
+        /// <summary>
+        /// Add two integers.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>Sum of the two integers.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
