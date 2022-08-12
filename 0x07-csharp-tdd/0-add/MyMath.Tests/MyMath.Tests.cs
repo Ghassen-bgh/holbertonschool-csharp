11db60cf873a3a4f11d98ba61fe76482617ebf09
Ghassen-bgh <3591@holbertonschool.com>
2022-08-12 15:53:49 +0200

    fix

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,19 +1,19 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+
 
         [Test]
         [TestCase(4, 6)]
         [TestCase(3, 7)]
         [TestCase(5, 5)]
         [TestCase(10, 0)]
+        [TestCase(0, 0)]
+        [TestCase(0, 10)]
+        [TestCase(-10, 20)]
         [TestCase(20, -10)]
 
         public void Addition(int a, int b)
