4d7f4b228aecded82643991bae4a6aa2831f2d77
Ghassen-bgh <3591@holbertonschool.com>
2022-08-15 14:24:31 +0200

    type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+    class Obj
+    {
+        public static void Print(object myObj)
+        {
+            string string_type = myObj.GetType().Name;
+            Type type = myObj.GetType();
+            Console.WriteLine("{0} Properties:", string_type);
+            foreach (var property in type.GetProperties())
+                Console.WriteLine(property.Name);
+            Console.WriteLine("{0} Methods:", string_type);
+            foreach (var method in type.GetMethods())
+                Console.WriteLine(method.Name);
+        }
+    }
+
