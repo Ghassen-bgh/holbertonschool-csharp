d22cb215a0cbbad91a9b0927417898af5fbb3b8f
Ghassen-bgh <3591@holbertonschool.com>
2022-09-06 11:49:41 +0200

    Magnitude

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+    class VectorMath
+    {
+        public static double Magnitude(double[] vector)
+        {
+            double sum = 0;
+            if (vector.Length == 2)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    sum += Math.Pow(vector[i], 2);
+                }
+                return Math.Round(Math.Sqrt(sum), 2);
+            }
+            else if (vector.Length == 3)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    sum += Math.Pow(vector[i], 2);
+                }
+                return Math.Round(Math.Sqrt(sum), 2);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
