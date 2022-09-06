51af56446fa3207165f17818f48e8c439b4e57c1
Ghassen-bgh <3591@holbertonschool.com>
2022-09-06 11:36:36 +0100

    Vector addition fix

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -6,7 +6,15 @@
         public static double[] Add(double[] vector1, double[] vector2)
         {
             double[] result = new double[vector1.Length];
-            if (vector1.Length == vector2.Length && vector1.Length < 4)
+            if (vector1.Length == vector2.Length && vector1.Length == 2)
+            {
+                for (int i = 0; i < vector1.Length; i++)
+                {
+                    result[i] = vector1[i] + vector2[i];
+                }
+                return result;
+            }
+            else if (vector1.Length == vector2.Length && vector1.Length == 3)
             {
                 for (int i = 0; i < vector1.Length; i++)
                 {
