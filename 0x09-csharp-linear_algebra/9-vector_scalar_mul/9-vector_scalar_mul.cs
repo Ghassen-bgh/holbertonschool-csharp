72cade3f4ffa1b0bd044c3912e890980abdcc626
Ghassen-bgh <3591@holbertonschool.com>
2022-09-06 15:06:10 +0100

    Vector-scalar multiplication

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+    class VectorMath
+    {
+        public static double[] Multiply(double[] vector, double scalar)
+        {
+            double[] result = new double[vector.Length];
+            if (vector.Length == 2)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    result[i] = vector[i] * scalar;
+                }
+                return result;
+            }
+            else if (vector.Length == 3)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    result[i] = vector[i] * scalar;
+                }
+                return result;
+            }
+            else
+            {
+                return new double[] {-1};
+            }
+        }
+    }
+
