fae95b4d6cb235e1df76eeb331f6ff2d15137284
Ghassen-bgh <3591@holbertonschool.com>
2022-09-08 15:05:23 +0100

    Matrix-scalar multiplication

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+    class MatrixMath
+    {
+        public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            double[,] result = new double[rows, cols];
+            if (rows != cols)
+            {
+                return (new Double[,] { { -1 } });
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = matrix[i, j] * scalar;
+                }
+            }
+            return result;
+        }
+    }
+
