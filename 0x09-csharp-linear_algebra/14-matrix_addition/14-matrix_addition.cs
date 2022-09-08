f2315295b0ec08da077d2e7b9d2f5fd7bd06ff72
Ghassen-bgh <3591@holbertonschool.com>
2022-09-08 15:01:39 +0100

    Matrix addition fix

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -5,15 +5,25 @@
     {
         public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
-            double[,] result = new double[,] {{-1}};
-            int rows = matrix1.GetLength(0);
-            int cols = matrix1.GetLength(1);
-            if (matrix1.Length != matrix2.Length || rows != matrix2.GetLength(0) || cols != matrix2.GetLength(1))
+
+            int rows1 = matrix1.GetLength(0);
+            int cols1 = matrix1.GetLength(1);
+            int rows2 = matrix2.GetLength(0);
+            int cols2 = matrix2.GetLength(1);
+            /*if (matrix1.Length != matrix2.Length || rows != matrix2.GetLength(0) || cols != matrix2.GetLength(1))
                 return result;
-            result = new double[rows, cols];
-            for (int i = 0; i < rows; i++)
+            result = new double[rows, cols];*/
+            if (matrix1.Length != matrix2.Length ||
+                rows1 != cols1 ||
+                rows2 != cols2)
             {
-                for (int j = 0; j < cols; j++)
+                return (new Double[,] { { -1 } });
+            }
+
+        double[,] result = new double[rows1, cols1];
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols1; j++)
                 {
                     result[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
