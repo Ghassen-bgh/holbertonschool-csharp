73fd18e6203c1a8113c85def299b1f17134be470
Ghassen-bgh <3591@holbertonschool.com>
2022-09-08 15:14:31 +0100

    Matrix-matrix multiplication

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    class MatrixMath
+    {
+        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+        {
+            double[,] result = new double[,] {{-1}};
+            int row1 = matrix1.GetLength(0);
+            int col1 = matrix1.GetLength(1);
+            int row2 = matrix2.GetLength(0);
+            int col2 = matrix2.GetLength(1);
+            if (col1 != row2)
+                return result;
+            result = new double[row1, col2];
+            for (int i = 0; i < row1; i++)
+            {
+                for (int j = 0; j < col2; j++)
+                {
+                    for (int k = 0; k < col1; k++)
+                    {
+                        result[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
