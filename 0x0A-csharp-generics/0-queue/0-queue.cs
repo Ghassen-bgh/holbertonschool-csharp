6ad90f59093973a79facf76b403c5a796b4f2e46
Ghassen-bgh <3591@holbertonschool.com>
2022-11-07 14:50:09 +0100

    Queue

---
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary>Queue class</summary>
+/// <typeparam name="T">Type of the queue</typeparam>
+    class Queue<T>
+    {
+        public Type CheckType()
+        {
+            return typeof(T);
+        }
+    }
