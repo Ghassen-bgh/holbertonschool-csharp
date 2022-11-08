c19d66e2f2d4bac9e723ae8f8319c31b37ba334f
Ghassen-bgh <3591@holbertonschool.com>
2022-11-08 21:35:05 +0100

    user interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,50 @@
+﻿using System;
+/// <summary>
+/// Abstract class
+/// </summary>
+
+    abstract class Base
+    {
+        public string name {get; set; }
+
+        public override string ToString()
+        {
+            return $"{name} is a {GetType().Name}";
+        }
+    }
+
+    public interface IInteractive
+    {
+        void Interact();
+    }
+
+    public interface IBreakable
+    {
+        int durability { get; set; }
+        void Break();
+    }
+
+    public interface ICollectable
+    {
+        bool isCollected{ get; set; }
+        void Collect(); 
+    }
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+    }
+    
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
+
