049b84ce7e4824be61f2b42318b617568cde82f5
Ghassen-bgh <3591@holbertonschool.com>
2022-11-10 15:49:11 +0100

    univ health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Class Player
+/// </summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name">Name of the player</param>
+    /// <param name="maxHp">Max health of the player</param>
+    public Player(string name ="Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Print the health of the player
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+}
